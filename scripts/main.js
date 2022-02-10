60f1afdcaf0ee88072ef8cb73e27bf774e681ce9
Matthew Stroup <mjstroup@purdue.edu>
2022-02-10 06:39:41 -0500

    added update_div function and button functionality

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,67 @@ function generate() {
 arrayElement.addEventListener("input", update);
 function update() {
     array_size = arrayElement.value;
+    delay = 100/array_size;
     generate();
 }
 
 window.onload = update();
+
+var algos = document.querySelectorAll(".buttons button");
+
+for (var i = 0; i < algos.length; i++) {
+    algos[i].addEventListener("click", run);
+}
+
+function disable() {
+    //disable everything on gui
+    for (var i = 0; i < algos.length; i++) {
+        algos[i].disabled = true;
+    }
+    arrayElement.disabled = true;
+    reset_array.disabled = true;
+}
+
+function enable() {
+    window.setTimeout(() => {
+        //enable everything on gui
+        for (var i = 0; i < algos.length; i++) {
+            algos[i].disabled = false;
+        }
+        arrayElement.disabled = false;
+        reset_array.disabled = false;
+    },delayTotal+=delay);
+}
+
+function run() {
+    disable();
+
+    switch (this.innerHTML) {
+        case 'Bubble Sort':
+            bubble();
+            break
+        case 'Insertion Sort':
+            insertion();
+            break;
+        case 'Merge Sort':
+            merge();
+            break;
+        case 'Quick Sort':
+            quick();
+            break;
+        case 'Selection Sort':
+            selection();
+            break;
+    }
+    enable();
+}
+
+var delay = 100/array_size;
+var delayTotal = 0;
+//to be called in each sort when we want to change values of each div
+function update_div(div, height, color) {
+    window.setTimeout(() => {
+        div.style="background-color:" + color +"; margin:0% 0.1%; width:" + (100/array_size) + "%; height:" + height + "%";
+    }, delayTotal);
+    delayTotal+=delay;
+}
